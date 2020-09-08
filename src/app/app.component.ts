cf7915cf81ee5a155fc9ee075be92dd56a0827cf
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-08 17:44:32 +0530

    Todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-todo-list';
+  /* An empty array that is responsible 
+	to add a division */
+  public items = [];
+
+  /* A two-way binding performed which 
+	pushes text on division */
+  public newTask;
+
+  /* When input is empty, it will 
+	not create a new division */
+  public addToList() {
+    if (this.newTask == '') {
+      // (<HTMLInputElement>document.querySelector('.btn-light')).disabled = true;
+    } else {
+      this.items.push(this.newTask);
+      this.newTask = '';
+    }
+  }
+
+  /* This function takes to input the 
+	task, that has to be deleted*/
+  public deleteTask(index) {
+    this.items.splice(index, 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [FormsModule, ReactiveFormsModule, BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
